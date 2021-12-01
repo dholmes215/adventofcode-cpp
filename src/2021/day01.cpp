2cecaad548dcb7ab891ff340692e089cb8b83902
David Holmes <dholmes@dholmes.us>
2021-12-01 03:00:18 -0600

    [2021-01] 2021 Day 1 input validation & cleanup

--- src/2021/day01.cpp
+++ src/2021/day01.cpp
@@ -8,32 +8,42 @@
 #include <aoc.hpp>
 #include <aoc_range.hpp>
 
+#include <fmt/format.h>
+
 #include <charconv>
 
 namespace aoc::year2021 {
 
+namespace {
+
 int to_int(std::string_view sv)
 {
     int out{0};
-    std::from_chars(&*sv.begin(), &*sv.end(), out);
+    const auto result{std::from_chars(&*sv.begin(), &*sv.end(), out)};
+    if (result.ec != std::errc{}) {
+        throw input_error{
+            fmt::format("error parsing \"{}\" as int: {}", sv, result.ec)};
+    }
     return out;
 }
 
-bool back_greater_than_front(auto&& rng)
+bool back_greater_than_front(auto&& rng) noexcept
 {
     return *r::prev(rng.end()) > r::front(rng);
 }
+
+}  // namespace
 
 aoc::solution_result day01(std::string_view input)
 {
     const auto depths{sv_lines(input) | rv::transform(to_int) |
                       r::to<std::vector>()};
 
-    auto part_a{r::count_if(depths | rv::sliding(2), [](auto&& rng) {
+    const auto part_a{r::count_if(depths | rv::sliding(2), [](auto&& rng) {
         return back_greater_than_front(rng);
     })};
 
-    auto part_b{r::count_if(depths | rv::sliding(4), [](auto&& rng) {
+    const auto part_b{r::count_if(depths | rv::sliding(4), [](auto&& rng) {
         return back_greater_than_front(rng);
     })};
 
