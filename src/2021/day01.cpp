f1f33ce581a3ade293cfa743f55915916424be01
David Holmes <dholmes@dholmes.us>
2021-12-01 17:03:16 -0600

    [2021-01] 2021 Day 1 polish

--- src/2021/day01.cpp
+++ src/2021/day01.cpp
@@ -16,17 +16,28 @@ namespace {
 
 std::vector<int> parse_input(std::string_view input)
 {
-    // return int_lines(input) | r::to<std::vector>();
-
     std::vector<int> depths;
     depths.reserve(2048);
     r::copy(int_lines(input), r::back_inserter(depths));
     return depths;
 }
 
-bool back_greater_than_front(auto&& rng) noexcept
+auto count_depth_increases(const std::vector<int>& v, int window_size) noexcept
 {
-    return *r::prev(rng.end()) > r::front(rng);
+    // Part B explanation: the problem asks us to compare the sums of the
+    // elements of overlapping windows of size 3:
+    //   (a + b + c) < (b + c + d)
+    // Subtract (b + c) from both sides and this can be simplified to:
+    //   a < d
+    // Thus instead of comparing two windows of size 3, we can compare the first
+    // and last elements of one window of size 4.  Part A involved comparing the
+    // first and last elements of one window of size 2, so the two parts are
+    // really the same general problem with different window sizes.
+
+    const auto windows{v | rv::sliding(window_size)};
+    using window_t = decltype(windows[0]);
+    auto depth_increased{[=](window_t w) { return w[window_size - 1] > w[0]; }};
+    return r::count_if(windows, depth_increased);
 }
 
 }  // namespace
@@ -34,15 +45,8 @@ bool back_greater_than_front(auto&& rng) noexcept
 aoc::solution_result day01(std::string_view input)
 {
     const std::vector<int> depths{parse_input(input)};
-
-    const auto part_a{r::count_if(depths | rv::sliding(2), [](auto&& rng) {
-        return back_greater_than_front(rng);
-    })};
-
-    const auto part_b{r::count_if(depths | rv::sliding(4), [](auto&& rng) {
-        return back_greater_than_front(rng);
-    })};
-
+    const auto part_a{count_depth_increases(depths, 2)};
+    const auto part_b{count_depth_increases(depths, 4)};
     return {part_a, part_b};
 }
 
