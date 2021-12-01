479e4fc71c60d54370f5bff741ec989c200f563f
David Holmes <dholmes@dholmes.us>
2021-12-01 01:00:21 -0600

    [2021-01] 2021 Day 1 Take 1

--- src/2021/day01.cpp
+++ src/2021/day01.cpp
@@ -6,12 +6,50 @@
 //
 
 #include <aoc.hpp>
+#include <aoc_range.hpp>
+
+#include <fmt/format.h>
+
+#include <cstdlib>
+#include <numeric>
 
 namespace aoc::year2021 {
 
-aoc::solution_result day01(std::string_view /*input*/)
+using depth = int;
+
+depth to_int(std::string_view sv)
 {
-    return {};
+    return std::atoi(sv.data());
+}
+
+int is_positive(depth d)
+{
+    return d > 0 ? 1 : 0;
+}
+
+depth difference(auto&& rng)
+{
+    return *(++rng.begin()) - *rng.begin();
+}
+
+aoc::solution_result day01(std::string_view input)
+{
+    const auto depths{sv_lines(input) | rv::transform(to_int) |
+                      r::to<std::vector>()};
+
+    auto part_a{r::count_if(
+        depths | rv::sliding(2) |
+            rv::transform([](auto&& rng) { return difference(rng); }),
+        is_positive)};
+
+    auto part_b{r::count_if(
+        depths | rv::sliding(3) | rv::transform([](auto&& rng) {
+            return r::accumulate(rng, 0);
+        }) | rv::sliding(2) |
+            rv::transform([](auto&& rng) { return difference(rng); }),
+        is_positive)};
+
+    return {part_a, part_b};
 }
 
 }  // namespace aoc::year2021
