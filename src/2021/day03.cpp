cd329155fc73d4e4c07ed96fb340e1012e86df76
David Holmes <dholmes@dholmes.us>
2021-12-03 01:00:27 -0600

    [2021-03] Part B functional

--- src/2021/day03.cpp
+++ src/2021/day03.cpp
@@ -22,7 +22,7 @@ int digit_to_int(char c)
     return c - '0';
 }
 
-std::vector<int> sv_to_vector(std::string_view sv)
+std::vector<int> sv_to_ints(std::string_view sv)
 {
     return sv | rv::transform(digit_to_int) | r::to<std::vector>;
 }
@@ -59,7 +59,7 @@ std::string binary_range_to_string(auto&& r)
 
 aoc::solution_result day03(std::string_view input)
 {
-    const auto rows{sv_lines(input) | rv::transform(sv_to_vector) |
+    const auto rows{sv_lines(input) | rv::transform(sv_to_ints) |
                     r::to<std::vector>};
     const auto position_count{rows[0].size()};
     const int rows_count{static_cast<int>(rows.size())};
@@ -78,7 +78,72 @@ aoc::solution_result day03(std::string_view input)
 
     fmt::print("gamma rate: {} {}\n", gamma_rate, epsilon_rate_int);
 
-    return {power_consumption, 0};
+    // Part B
+
+    int oxygen_generator_rating{};
+    {
+        std::vector<std::vector<int>> rows_oxygen{rows.begin(), rows.end()};
+        for (std::size_t i{0}; i < position_count; i++) {
+            auto rows_oxygen_count{rows_oxygen.size()};
+            auto count_ones{r::accumulate(
+                rows_oxygen | rv::transform([=](const std::vector<int>& r) {
+                    return r[i];
+                }),
+                0)};
+            int count_zeroes{static_cast<int>(rows_oxygen_count) - count_ones};
+            int most_common{count_ones >= count_zeroes ? 1 : 0};
+            std::vector<std::vector<int>> filtered{
+                rows_oxygen |
+                rv::filter([=](auto&& r) { return r[i] == most_common; }) |
+                r::to<std::vector>};
+            std::swap(rows_oxygen, filtered);
+            fmt::print("{}: filtered down to {} rows\n", i, rows_oxygen.size());
+            if (rows_oxygen.size() == 1) {
+                break;
+            }
+        }
+
+        auto selected = rows_oxygen[0];
+        oxygen_generator_rating = binary_range_to_int(
+            selected | rv::transform([](int i) { return i == 1; }));
+
+        fmt::print("oxygen generator: {}\n", oxygen_generator_rating);
+    }
+
+    int co2_scrubber_rating{};
+    {
+        std::vector<std::vector<int>> rows_oxygen{rows.begin(), rows.end()};
+        for (std::size_t i{0}; i < position_count; i++) {
+            auto rows_oxygen_count{rows_oxygen.size()};
+            auto count_ones{r::accumulate(
+                rows_oxygen | rv::transform([=](const std::vector<int>& r) {
+                    return r[i];
+                }),
+                0)};
+            int count_zeroes{static_cast<int>(rows_oxygen_count) - count_ones};
+            int least_common{count_ones < count_zeroes ? 1 : 0};
+            std::vector<std::vector<int>> filtered{
+                rows_oxygen |
+                rv::filter([=](auto&& r) { return r[i] == least_common; }) |
+                r::to<std::vector>};
+            std::swap(rows_oxygen, filtered);
+            fmt::print("{}: filtered down to {} rows\n", i, rows_oxygen.size());
+            if (rows_oxygen.size() == 1) {
+                break;
+            }
+        }
+
+        auto selected = rows_oxygen[0];
+        co2_scrubber_rating = binary_range_to_int(
+            selected | rv::transform([](int i) { return i == 1; }));
+
+        fmt::print("co2 scrubber: {}\n", co2_scrubber_rating);
+    }
+
+    const int life_support_rating{oxygen_generator_rating *
+                                  co2_scrubber_rating};
+
+    return {power_consumption, life_support_rating};
 }
 
 }  // namespace aoc::year2021
