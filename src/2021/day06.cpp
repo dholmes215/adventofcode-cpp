1766eb37db5e57fa23f2372465d2a02152cfdf47
David Holmes <dholmes@dholmes.us>
2021-12-05 23:57:58 -0600

    [2015-06] Day 6 seasoning!

--- src/2021/day06.cpp
+++ src/2021/day06.cpp
@@ -8,6 +8,8 @@
 #include <aoc.hpp>
 #include <aoc_range.hpp>
 
+#include <vector>
+
 namespace aoc::year2021 {
 
 namespace {
@@ -17,12 +19,21 @@ using count_type = std::int64_t;
 constexpr timer cycle_length{7};
 constexpr timer initial_length{9};
 
+std::vector<count_type> populate_initial_counts(
+    const std::vector<timer>& fish) noexcept
+{
+    std::vector<count_type> counts_by_timer(initial_length, 0);
+    for (timer number : fish) {
+        counts_by_timer[static_cast<std::size_t>(number)]++;
+    }
+    return counts_by_timer;
+}
+
 void run_days(std::vector<count_type>& counts, int days) noexcept
 {
     for (int i{0}; i < days; i++) {
         r::rotate(counts, counts.begin() + 1);
         counts[cycle_length - 1] += counts[initial_length - 1];
-
     }
 };
 
@@ -31,11 +42,7 @@ void run_days(std::vector<count_type>& counts, int days) noexcept
 aoc::solution_result day06(std::string_view input)
 {
     const auto fish{numbers<timer>(input) | r::to<std::vector>};
-
-    std::vector<count_type> counts_by_timer(initial_length, 0);
-    for (timer number : fish) {
-        counts_by_timer[static_cast<std::size_t>(number)]++;
-    }
+    std::vector<count_type> counts_by_timer(populate_initial_counts(fish));
 
     run_days(counts_by_timer, 80);
     const auto sum_a{r::accumulate(counts_by_timer, count_type{0})};
