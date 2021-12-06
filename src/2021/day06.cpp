10cdf9254ce060a135069008581c797e293b68a3
David Holmes <dholmes@dholmes.us>
2021-12-06 00:10:38 -0600

    [2015-06] Additional seasoning

--- src/2021/day06.cpp
+++ src/2021/day06.cpp
@@ -14,40 +14,47 @@ namespace aoc::year2021 {
 
 namespace {
 
-using timer = int;
+using timer_int = int;
 using count_type = std::int64_t;
-constexpr timer cycle_length{7};
-constexpr timer initial_length{9};
+constexpr timer_int cycle_length{7};
+constexpr timer_int initial_length{9};
+constexpr int part1_days{80};
+constexpr int part2_days{256};
 
 std::vector<count_type> populate_initial_counts(
-    const std::vector<timer>& fish) noexcept
+    const std::vector<timer_int>& fish) noexcept
 {
     std::vector<count_type> counts_by_timer(initial_length, 0);
-    for (timer number : fish) {
+    for (timer_int number : fish) {
         counts_by_timer[static_cast<std::size_t>(number)]++;
     }
     return counts_by_timer;
 }
 
+void run_day(std::vector<count_type>& counts) noexcept
+{
+    r::rotate(counts, counts.begin() + 1);
+    counts[cycle_length - 1] += counts[initial_length - 1];
+}
+
 void run_days(std::vector<count_type>& counts, int days) noexcept
 {
     for (int i{0}; i < days; i++) {
-        r::rotate(counts, counts.begin() + 1);
-        counts[cycle_length - 1] += counts[initial_length - 1];
+        run_day(counts);
     }
-};
+}
 
 }  // namespace
 
 aoc::solution_result day06(std::string_view input)
 {
-    const auto fish{numbers<timer>(input) | r::to<std::vector>};
+    const auto fish{numbers<timer_int>(input) | r::to<std::vector>};
     std::vector<count_type> counts_by_timer(populate_initial_counts(fish));
 
-    run_days(counts_by_timer, 80);
+    run_days(counts_by_timer, part1_days);
     const auto sum_a{r::accumulate(counts_by_timer, count_type{0})};
 
-    run_days(counts_by_timer, 176);
+    run_days(counts_by_timer, part2_days - part1_days);
     const auto sum_b{r::accumulate(counts_by_timer, count_type{0})};
 
     return {sum_a, sum_b};
