a2e4fb324619cc2b9643ef41415e52fec3ac4c90
David Holmes <dholmes@dholmes.us>
2021-12-05 23:45:36 -0600

    [2015-06] Day 6 polish

--- src/2021/day06.cpp
+++ src/2021/day06.cpp
@@ -12,12 +12,21 @@ namespace aoc::year2021 {
 
 namespace {
 
-}  // namespace
-
 using timer = int;
 using count_type = std::int64_t;
 constexpr timer cycle_length{7};
 constexpr timer initial_length{9};
+
+void run_days(std::vector<count_type>& counts, int days) noexcept
+{
+    for (int i{0}; i < days; i++) {
+        r::rotate(counts, counts.begin() + 1);
+        counts[cycle_length - 1] += counts[initial_length - 1];
+
+    }
+};
+
+}  // namespace
 
 aoc::solution_result day06(std::string_view input)
 {
@@ -29,19 +38,10 @@ aoc::solution_result day06(std::string_view input)
         counts_by_timer[static_cast<std::size_t>(number)]++;
     }
 
-    auto run_days{[&](int days){
-        for (int i = 0; i < days; i++) {
-            r::rotate(counts_by_timer, counts_by_timer.begin() + 1);
-            counts_by_timer[cycle_length - 1] +=
-                counts_by_timer[initial_length - 1];
-
-        }
-    }};
-
-    run_days(80);
+    run_days(counts_by_timer, 80);
     const auto sum_a{r::accumulate(counts_by_timer, count_type{0})};
 
-    run_days(176);
+    run_days(counts_by_timer, 176);
     const auto sum_b{r::accumulate(counts_by_timer, count_type{0})};
 
     return {sum_a, sum_b};
