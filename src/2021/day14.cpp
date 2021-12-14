6262b54567d990a8211446b6e3a6b67114b0db97
David Holmes <dholmes@dholmes.us>
2021-12-13 22:56:43 -0600

    [2021-14] Day 14 skeleton

---
+++ src/2021/day14.cpp
@@ -0,0 +1,27 @@
+//
+// Copyright (c) 2020-2021 David Holmes (dholmes at dholmes dot us)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <aoc.hpp>
+#include <aoc_range.hpp>
+
+#include <fmt/format.h>
+
+#include <string_view>
+
+namespace aoc::year2021 {
+
+namespace {
+
+}  // namespace
+
+aoc::solution_result day14(std::string_view input)
+{
+    (void)input;
+    return {};
+}
+
+}  // namespace aoc::year2021
--- src/aoc_solutions.cpp
+++ src/aoc_solutions.cpp
@@ -48,7 +48,8 @@ const std::map<date, std::vector<solution>>& solutions()
         {{2021, 10}, {{aoc::year2021::day10, ""}}},
         {{2021, 11}, {{aoc::year2021::day11, ""}}},
         {{2021, 12}, {{aoc::year2021::day12, ""}}},
-        {{2021, 13}, {{aoc::year2021::day13, ""}}}};
+        {{2021, 13}, {{aoc::year2021::day13, ""}}},
+        {{2021, 14}, {{aoc::year2021::day14, ""}}}};
     return solutions;
 }
 
