f06391edb8bf0fa831c0c08d663dfab41e9cc6cb
David Holmes <dholmes@dholmes.us>
2021-12-16 22:58:52 -0600

    [2021-17] Day 17 skeleton

---
+++ src/2021/day17.cpp
@@ -0,0 +1,24 @@
+//
+// Copyright (c) 2020-2021 David Holmes (dholmes at dholmes dot us)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <aoc.hpp>
+#include <aoc_range.hpp>
+
+#include <fmt/format.h>
+
+#include <string_view>
+#include <vector>
+
+namespace aoc::year2021 {
+
+aoc::solution_result day17(std::string_view input)
+{
+    (void)input;
+    return {};
+}
+
+}  // namespace aoc::year2021
