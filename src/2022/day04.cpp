4a68940beab35317a7fda27ccf8614691a0a7844
David Holmes <dholmes@dholmes.us>
2022-12-03 23:25:38 -0600

    [2022-04] Solution

--- src/2022/day04.cpp
+++ src/2022/day04.cpp
@@ -6,19 +6,64 @@
 //
 
 #include <aoc.hpp>
+#include <aoc_range.hpp>
+
+#include <ctre.hpp>
+
+#include <fmt/format.h>
 
 #include <string_view>
+#include <utility>
 
 namespace aoc::year2022 {
 
 namespace {
 
+using number_t = std::uint8_t;
+using range_t = std::pair<number_t, number_t>;
+using pair_t = std::pair<range_t, range_t>;
+
+pair_t parse_line(std::string_view line)
+{
+    constexpr auto matcher{ctre::match<R"((\d+)-(\d+),(\d+)-(\d+))">};
+    if (auto [whole, a1, a2, b1, b2] = matcher(line); whole) {
+        return {{to_num<number_t>(a1), to_num<number_t>(a2)},
+                {to_num<number_t>(b1), to_num<number_t>(b2)}};
+    }
+    throw input_error{fmt::format("failed to parse input: {}", line)};
+}
+
+constexpr bool contains(range_t a, range_t b)
+{
+    return a.first <= b.first && a.second >= b.second;
+}
+
+constexpr bool either_contains(pair_t pair)
+{
+    return contains(pair.first, pair.second) ||
+           contains(pair.second, pair.first);
+}
+
+constexpr bool contains_end(range_t a, range_t b)
+{
+    return (a.first >= b.first && a.first <= b.second) ||
+           (b.first >= a.first && b.first <= a.second);
+}
+
+constexpr bool overlaps(pair_t pair)
+{
+    return contains_end(pair.first, pair.second) ||
+           contains_end(pair.second, pair.first);
+}
+
 }  // namespace
 
 aoc::solution_result day04(std::string_view input)
 {
-    input = trim(input);
-    return {"", ""};
+    const auto pairs{sv_lines(trim(input)) | rv::transform(parse_line) |
+                     r::to<std::vector>};
+
+    return {r::count_if(pairs, either_contains), r::count_if(pairs, overlaps)};
 }
 
 }  // namespace aoc::year2022
--- src/aoc_solutions.cpp
+++ src/aoc_solutions.cpp
@@ -84,6 +84,7 @@ const std::map<date, std::vector<solution>>& solutions()
          {{aoc::year2022::day02naive, "naive"},
           {aoc::year2022::day02lookup, "lookup"}}},
         {{2022, 03}, {{aoc::year2022::day03, ""}}},
+        {{2022, 04}, {{aoc::year2022::day04, ""}}},
     };
     return solutions;
 }
