8e865dd6f6fdb960c6b396c626b2a506e2f48317
David Holmes <dholmes@dholmes.us>
2023-12-08 01:11:02 -0600

    [2023-08] Use std::lcm instead of own

--- src/2023/day08.cpp
+++ src/2023/day08.cpp
@@ -10,6 +10,7 @@
 
 #include <algorithm>
 #include <map>
+#include <numeric>
 #include <string_view>
 #include <vector>
 
@@ -29,33 +30,17 @@ int instruction_as_index(char instruction)
     throw input_error(fmt::format("Invalid instruction: {}\n", instruction));
 }
 
-using map_t = std::unordered_map<std::string_view, std::array<std::string_view, 2>>;
+using map_t =
+    std::unordered_map<std::string_view, std::array<std::string_view, 2>>;
 
 map_t::value_type parse_node(std::string_view line)
 {
     return {line.substr(0, 3), {line.substr(7, 3), line.substr(12, 3)}};
 }
 
-std::int64_t gcd(std::int64_t a, std::int64_t b) {
-    while (b != 0) {
-        std::int64_t temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-
-std::int64_t lcm(std::int64_t a, std::int64_t b) {
-    return (a * b) / gcd(a, b);
-}
-
-std::int64_t lcm_vector(const std::vector<int>& numbers) {
-    std::int64_t result = numbers[0];
-    for (size_t i = 1; i < numbers.size(); ++i) {
-        result = lcm(result, numbers[i]);
-    }
-
-    return result;
+std::int64_t lcm_vector(const std::vector<std::int64_t>& numbers)
+{
+    return r::accumulate(numbers, 1LL, std::lcm<std::int64_t, std::int64_t>);
 }
 
 }  // namespace
@@ -73,7 +58,7 @@ aoc::solution_result day08(std::string_view input)
 
     const auto solve_part1{[&](std::string_view start_node,
                                auto&& is_end_predicate) {
-        int part1_steps_taken{0};
+        std::int64_t part1_steps_taken{0};
         std::string_view part1_current_node{start_node};
 
         auto part1_current_instruction_iter{r::begin(cycled_idx_instructions)};
@@ -85,9 +70,8 @@ aoc::solution_result day08(std::string_view input)
         return part1_steps_taken;
     }};
 
-    int part1{solve_part1("AAA",
-                          [](std::string_view node) { return node == "ZZZ";
-                          })};
+    std::int64_t part1{solve_part1(
+        "AAA", [](std::string_view node) { return node == "ZZZ"; })};
 
     const auto is_start_node{
         [](std::string_view node) { return node[2] == 'A'; }};
@@ -97,7 +81,7 @@ aoc::solution_result day08(std::string_view input)
         nodes | rv::keys | rv::filter(is_start_node) | r::to<std::vector>};
     const auto part2_transform_func{
         [&](std::string_view key) { return solve_part1(key, is_end_node); }};
-    const std::vector<int> part2_steps_taken{
+    const std::vector<std::int64_t> part2_steps_taken{
         part2_start_nodes | rv::transform(part2_transform_func) |
         r::to<std::vector>};
 
