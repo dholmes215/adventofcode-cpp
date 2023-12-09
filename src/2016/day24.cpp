c0170e8d0641aa01d7966eea3bf2f67dcb80c83b
David Holmes <dholmes@dholmes.us>
2023-12-09 15:34:50 -0600

    [2016] Add skeletons

---
+++ src/2016/day24.cpp
@@ -0,0 +1,29 @@
+//
+// Copyright (c) 2020-2023 David Holmes (dholmes at dholmes dot us)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <aoc.hpp>
+#include <aoc_range.hpp>
+
+#include <fmt/ranges.h>
+
+#include <algorithm>
+#include <string_view>
+#include <vector>
+
+namespace aoc::year2016 {
+
+namespace {
+
+}  // namespace
+
+aoc::solution_result day24(std::string_view input)
+{
+    input = trim(input);
+    return {"", ""};
+}
+
+}  // namespace aoc::year2016
