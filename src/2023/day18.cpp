bd39369ac88df7e7f20bfa70e41cac227d11bf40
David Holmes <dholmes@dholmes.us>
2023-12-18 02:31:55 -0600

    [2023-18] Solution

--- src/2023/day18.cpp
+++ src/2023/day18.cpp
@@ -28,19 +28,6 @@ using big_int_t = std::int64_t;
 using grid_t = dynamic_grid<char>;
 using pos_t = vec2<int>;
 
-struct rgb {
-    std::uint8_t r;
-    std::uint8_t g;
-    std::uint8_t b;
-};
-
-rgb parse_color(std::string_view sv)
-{
-    return {to_num_base<std::uint8_t>(sv.substr(0, 2), 16),
-            to_num_base<std::uint8_t>(sv.substr(2, 2), 16),
-            to_num_base<std::uint8_t>(sv.substr(4, 2), 16)};
-}
-
 struct trench {
     pos_t direction;
     int_t meters;
@@ -146,8 +133,6 @@ struct start_parameters {
 
 start_parameters find_start_parameters(const std::vector<trench>& trenches)
 {
-    const pos_t start{0, 0};
-
     pos_t pos{0, 0};
     pos_t top_left{pos};
     pos_t bottom_right{pos};
@@ -164,215 +149,153 @@ start_parameters find_start_parameters(const std::vector<trench>& trenches)
 
 }  // namespace
 
-void print_grid(const grid_t& grid)
+big_int_t solve(const std::vector<trench>& trenches)
 {
-    for (int row{0}; row < grid.height(); row++) {
-        for (int col{0}; col < grid.width(); col++) {
-            fmt::print("{}", grid[{col, row}]);
+    const auto [start, dimensions]{find_start_parameters(trenches)};
+
+    std::vector<int_t> x_stopping_points{start.x};
+    std::vector<int_t> y_stopping_points{start.y};
+
+    {
+        pos_t pos{start};
+        for (const trench& t : trenches) {
+            pos += t.direction * t.meters;
+            x_stopping_points.push_back(pos.x);
+            y_stopping_points.push_back(pos.y);
         }
-        fmt::print("\n");
     }
-    fmt::print("\n");
+
+    r::sort(x_stopping_points);
+    x_stopping_points.erase(r::unique(x_stopping_points),
+                            x_stopping_points.end());
+    r::sort(y_stopping_points);
+    y_stopping_points.erase(r::unique(y_stopping_points),
+                            y_stopping_points.end());
+
+    // Map coordinates in the original coordinate system to a compressed
+    // grid.  The original coordinate will be used to look up the first key
+    // that's less-than or equal, and the corresponding value will be the
+    // new coordinate.
+    using compression_map_t = std::vector<std::pair<int_t, int_t>>;
+    const auto make_compression_map{
+        [](const std::vector<int_t>& stopping_points) {
+            compression_map_t compression_map{};
+            int_t compressed{0};
+            for (int_t i : stopping_points) {
+                if (!compression_map.empty()) {
+                    const auto& back{compression_map.back()};
+                    if (back.first == i) {
+                        // We pushed an extra last iteration
+                        compression_map.pop_back();
+                        compressed--;
+                    }
+                }
+                compression_map.push_back({i, compressed++});
+                compression_map.push_back({i + 1, compressed++});
+            }
+            compression_map.pop_back();
+            return compression_map;
+        }};
+
+    const auto x_compression_map{make_compression_map(x_stopping_points)};
+    const auto y_compression_map{make_compression_map(y_stopping_points)};
+
+    const auto compress_pos{[&](pos_t pos) {
+        auto x_keys{x_compression_map | rv::keys};
+        auto y_keys{y_compression_map | rv::keys};
+        auto x_key_iter{r::prev(r::upper_bound(x_keys, pos.x))};
+        auto y_key_iter{r::prev(r::upper_bound(y_keys, pos.y))};
+        auto x_dist{r::distance(x_keys.begin(), x_key_iter)};
+        auto y_dist{r::distance(y_keys.begin(), y_key_iter)};
+        return pos_t{x_compression_map[x_dist].second,
+                     y_compression_map[y_dist].second};
+    }};
+
+    // Map indexes in the compressed grid to the length to which the
+    // corresponded in the original coordinate system.
+    const auto make_expanded_lengths{
+        [](const compression_map_t& compression_map) {
+            std::vector<int_t> differences(compression_map.size());
+            r::adjacent_difference(compression_map | rv::keys,
+                                   differences.begin());
+            std::vector<int_t> expanded_lengths;
+            expanded_lengths.reserve(differences.size());
+            for (int_t diff : differences | rv::drop(1)) {
+                expanded_lengths.push_back(diff);
+            }
+            expanded_lengths.push_back(1);
+            return expanded_lengths;
+        }};
+
+    const auto x_expanded_lengths{make_expanded_lengths(x_compression_map)};
+    const auto y_expanded_lengths{make_expanded_lengths(y_compression_map)};
+
+    grid_t grid{static_cast<int>(x_expanded_lengths.size()),
+                static_cast<int>(y_expanded_lengths.size())};
+    r::fill(grid.data(), '.');
+    pos_t pos{start};
+    grid[compress_pos(pos)] = '#';
+
+    for (const trench& t : trenches) {
+        pos_t compressed{compress_pos(pos)};
+        pos_t new_pos{pos + t.direction * t.meters};
+        pos_t new_compressed(compress_pos(new_pos));
+
+        while (compressed != new_compressed) {
+            compressed += t.direction;
+            grid[compressed] = '#';
+        }
+
+        pos = new_pos;
+    }
+
+    // print_grid_braille(grid, [](char c) { return c != '.'; });
+
+    for (const trench& t : trenches) {
+        pos_t compressed{compress_pos(pos)};
+        pos_t new_pos{pos + t.direction * t.meters};
+        pos_t new_compressed(compress_pos(new_pos));
+
+        while (compressed != new_compressed) {
+            compressed += t.direction;
+            pos_t inside{compressed + right_hand_direction(t.direction)};
+            if (grid[inside] == '.') {
+                flood_fill(grid, inside, '#');
+            }
+        }
+
+        pos = new_pos;
+    }
+
+    // print_grid_braille(grid, [](char c) { return c != '.'; });
+
+    const auto expanded_area{[&](pos_t p) {
+        big_int_t len_x{x_expanded_lengths[p.x]};
+        big_int_t len_y{y_expanded_lengths[p.y]};
+        return len_x * len_y;
+    }};
+
+    big_int_t out{0};
+    for (pos_t p : grid.area().all_points()) {
+        if (grid[p] == '#') {
+            out += expanded_area(p);
+        }
+    }
+
+    return out;
 }
 
 aoc::solution_result day18(std::string_view input)
 {
-    big_int_t part1{};
-    {
-        const std::vector<trench> part1_trenches{
-            sv_lines(trim(input)) | rv::transform(part1_parse_trench) |
-            r::to<std::vector>};
+    const std::vector<trench> part1_trenches{sv_lines(trim(input)) |
+                                             rv::transform(part1_parse_trench) |
+                                             r::to<std::vector>};
+    big_int_t part1{solve(part1_trenches)};
 
-        // for (const trench& t : part1_trenches) {
-        //     fmt::print("{},{} {} \x1B[38;2;{};{};{}m{},{},{}\x1B[0m\n",
-        //                t.direction.x, t.direction.y, t.meters, t.color.r,
-        //                t.color.g, t.color.b, t.color.r, t.color.g,
-        //                t.color.b);
-        // }
-
-        const auto [start, dimensions]{find_start_parameters(part1_trenches)};
-
-        grid_t grid{dimensions.x, dimensions.y};
-        r::fill(grid.data(), '.');
-        pos_t pos{start};
-        grid[pos] = '#';
-
-        for (const trench& t : part1_trenches) {
-            for (int i{0}; i < t.meters; i++) {
-                pos += t.direction;
-                grid[pos] = '#';
-            }
-        }
-
-        // print_grid(grid);
-        // print_grid_braille(grid, [](char c) { return c != '.'; });
-
-        // pos = start;
-        // for (const trench& t : part1_trenches) {
-        //     for (int i{0}; i < t.meters; i++) {
-        //         pos += t.direction;
-        //         grid[pos] = '#';
-        //     }
-        // }
-
-        if (part1_trenches[0].direction != right) {
-            throw input_error("First direction must be R");
-        }
-
-        for (const trench& t : part1_trenches) {
-            for (int i{0}; i < t.meters; i++) {
-                pos += t.direction;
-                pos_t inside{pos + right_hand_direction(t.direction)};
-                if (grid[inside] == '.') {
-                    flood_fill(grid, inside, '#');
-                }
-            }
-        }
-
-        // print_grid_braille(grid, [](char c) { return c != '.'; });
-
-        part1 = r::count(grid.data(), '#');
-    }
-
-    //////////////////////////////////////////////
-
-    big_int_t part2{};
-    {
-        const std::vector<trench> part2_trenches{
-            sv_lines(trim(input)) | rv::transform(part2_parse_trench) |
-            r::to<std::vector>};
-
-        const auto [start, dimensions]{find_start_parameters(part2_trenches)};
-        fmt::print("Part 2 adjusted start: {},{}\n", start.x, start.y);
-
-        std::vector<int_t> x_stopping_points{{start.x}};
-        std::vector<int_t> y_stopping_points{{start.y}};
-
-        {
-            pos_t pos{start};
-            for (const trench& t : part2_trenches) {
-                pos += t.direction * t.meters;
-                x_stopping_points.push_back(pos.x);
-                y_stopping_points.push_back(pos.y);
-            }
-        }
-
-        r::sort(x_stopping_points);
-        x_stopping_points.erase(r::unique(x_stopping_points),
-                                x_stopping_points.end());
-        r::sort(y_stopping_points);
-        y_stopping_points.erase(r::unique(y_stopping_points),
-                                y_stopping_points.end());
-
-        fmt::print("x: {}\n", x_stopping_points);
-        fmt::print("y: {}\n", y_stopping_points);
-
-        // Map coordinates in the original coordinate system to a compressed
-        // grid.  The original coordinate will be used to look up the first key
-        // that's less-than or equal, and the corresponding value will be the
-        // new coordinate.
-        const auto make_compression_map{
-            [](const std::vector<int_t>& stopping_points) {
-                std::vector<std::pair<int_t, int_t>> compression_map{};
-                int_t compressed{0};
-                for (int_t i : stopping_points) {
-                    compression_map.push_back({i, compressed++});
-                    compression_map.push_back({i + 1, compressed++});
-                }
-                compression_map.pop_back();
-                return compression_map;
-            }};
-
-        const auto x_compression_map{make_compression_map(x_stopping_points)};
-        const auto y_compression_map{make_compression_map(y_stopping_points)};
-
-        fmt::print("x: {}\n", x_compression_map);
-        fmt::print("y: {}\n", y_compression_map);
-
-        const auto compress_pos{[&](pos_t pos) {
-            auto x_keys{x_compression_map | rv::keys};
-            auto y_keys{y_compression_map | rv::keys};
-            auto x_key_iter{r::prev(r::upper_bound(x_keys, pos.x))};
-            auto y_key_iter{r::prev(r::upper_bound(y_keys, pos.y))};
-            auto x_dist{r::distance(x_keys.begin(), x_key_iter)};
-            auto y_dist{r::distance(y_keys.begin(), y_key_iter)};
-            return pos_t{x_compression_map[x_dist].second,
-                         y_compression_map[y_dist].second};
-        }};
-
-        // Map indexes in the compressed grid to the length to which the
-        // corresponded in the original coordinate system.
-        const auto make_expanded_lengths{
-            [](const std::vector<int_t>& stopping_points) {
-                std::vector<int_t> differences(stopping_points.size());
-                r::adjacent_difference(stopping_points, differences.begin());
-                std::vector<int_t> expanded_lengths;
-                expanded_lengths.reserve(differences.size() * 2 + 1);
-                expanded_lengths.push_back(1);
-                for (int_t diff : differences | rv::drop(1)) {
-                    expanded_lengths.push_back(diff);
-                    expanded_lengths.push_back(1);
-                }
-                return expanded_lengths;
-            }};
-
-        const auto x_expanded_lengths{make_expanded_lengths(x_stopping_points)};
-        const auto y_expanded_lengths{make_expanded_lengths(y_stopping_points)};
-
-        fmt::print("x: {}\n", x_expanded_lengths);
-        fmt::print("y: {}\n", y_expanded_lengths);
-
-        grid_t grid{static_cast<int>(x_expanded_lengths.size()),
-                    static_cast<int>(y_expanded_lengths.size())};
-        r::fill(grid.data(), '.');
-        pos_t pos{start};
-        grid[compress_pos(pos)] = '#';
-
-        for (const trench& t : part2_trenches) {
-            pos_t compressed{compress_pos(pos)};
-            pos_t new_pos{pos + t.direction * t.meters};
-            pos_t new_compressed(compress_pos(new_pos));
-
-            while (compressed != new_compressed) {
-                compressed += t.direction;
-                grid[compressed] = '#';
-            }
-
-            pos = new_pos;
-        }
-
-        print_grid_braille(grid, [](char c) { return c != '.'; });
-
-        for (const trench& t : part2_trenches) {
-            pos_t compressed{compress_pos(pos)};
-            pos_t new_pos{pos + t.direction * t.meters};
-            pos_t new_compressed(compress_pos(new_pos));
-
-            while (compressed != new_compressed) {
-                compressed += t.direction;
-                pos_t inside{compressed + right_hand_direction(t.direction)};
-                if (grid[inside] == '.') {
-                    flood_fill(grid, inside, '#');
-                }
-            }
-
-            pos = new_pos;
-        }
-        
-        print_grid_braille(grid, [](char c) { return c != '.'; });
-
-        const auto expanded_area{[&](pos_t p) {
-            big_int_t len_x{x_expanded_lengths[p.x]};
-            big_int_t len_y{x_expanded_lengths[p.x]};
-            return len_x * len_y;
-        }};
-
-        for (pos_t p : grid.area().all_points()) {
-            if (grid[p] == '#') {
-                part2 += expanded_area(p);
-            }
-        }
-    }
+    const std::vector<trench> part2_trenches{sv_lines(trim(input)) |
+                                             rv::transform(part2_parse_trench) |
+                                             r::to<std::vector>};
+    big_int_t part2{solve(part2_trenches)};
 
     return {part1, part2};
 }
